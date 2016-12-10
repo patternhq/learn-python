e29a9ff2c42853975cd165a4f57e1572e6d6ddf2
Matthew Lee Gilson <matt@getpattern.com>
2016-12-09 19:41:46 -0800

    Add a hello-world example.

---
+++ hello-world/hello_world.py
@@ -0,0 +1,18 @@
+"""It is customary in programming to start with a "Hello World" example.  These
+examples usually just print something simple on the screen (like "Hello world").
+
+The text at the top of this file is considered a "docstring" (which is a
+special kind of comment in python).
+
+As you can see, a hello world program in python is trivial (it only takes one
+line of real code).
+"""
+
+# This is a normal comment.  It doesn't get executed or do anything.
+
+print('Hello World!')  # Comments also can happen at the end of the line.
+
+# In python3.x, `print` is a function which means that you need to call it.
+# In python2.x, `print` is a "keyword".  You'd use it like this:
+#
+# print 'Hello World!'
